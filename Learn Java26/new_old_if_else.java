289d70489e38eafdfc5eb609b7e7d9d809800df6
Rohitchand98 <rs486345@gmail.com>
2026-04-29 01:09:14 +0530

    if statement new statement

--- Learn Java26/new_old_if_else.java
+++ Learn Java26/new_old_if_else.java
@@ -1,7 +1,7 @@
 public class new_old_if_else {
     void main() {
         int x=0;
-        
+
         //Old If else statement 
         if(x==0){
             IO.println("x=="+x);
@@ -10,5 +10,26 @@ public class new_old_if_else {
         }else{
             IO.println("Invalid variable");
         }
-   }
+
+        //New if else statement 
+        if(x%2==0) IO.println("Even"); else IO.println("Odd");
+
+        //Or
+        if(x>=0) IO.println("its Positive"); 
+        else if(x<0) IO.println("its Negative"); 
+        else IO.println("Invalid input");
+
+        //New Nested if else statement
+        if(x==0){if(x==0) IO.println("This Number is Zero");}
+
+        //Object instanceof demo in if statement
+        Object obj=0;
+        if(obj instanceof Integer) IO.println("Object is "+obj);
+
+        //Json internal word search
+        String json="""
+                "id":1, "name":"Rohit",
+                """;
+        if(json.contains("Rohit")) IO.println("Name found "+json.contains("Rohit"));
+    }
 }
