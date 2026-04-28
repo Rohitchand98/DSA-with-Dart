afeabb1c91e96afcd2eb31f31b160507a5941643
Rohitchand98 <rs486345@gmail.com>
2026-04-28 23:19:13 +0530

    old if else statement

---
+++ Learn Java26/new_old_if_else.java
@@ -0,0 +1,14 @@
+public class new_old_if_else {
+    void main() {
+        int x=0;
+        
+        //Old If else statement 
+        if(x==0){
+            IO.println("x=="+x);
+        }else if(x!=0){
+            IO.println("x=="+x);
+        }else{
+            IO.println("Invalid variable");
+        }
+   }
+}
